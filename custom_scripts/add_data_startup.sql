edb7f21f11b148fee98e68de2f674de448e982bc
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2024-11-22 19:30:29 +0000

    Include an example to htable

--- custom_scripts/add_data_startup.sql
+++ custom_scripts/add_data_startup.sql
@@ -12,3 +12,5 @@ INSERT INTO dispatcher (setid, destination, description) values(1, 'sip:172.25.0
 INSERT INTO dispatcher (setid, destination, description) values(2, 'sip:172.25.0.4:5060', 'Registrar Server A');
 
 INSERT INTO dispatcher (setid, destination, description) values(2, 'sip:172.25.0.5:5060', 'Registrar Server B');
+
+INSERT INTO htable (key_name, "key_type", value_type, key_value, expires) VALUES('carrier_outbound_dispatch_by_matching_ruri', 0, 1, 1, 0);
