917b201b2ee44fec26355d63fd60fedc9326c3d6
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2025-06-28 18:33:47 +0100

    feat: add custom script to add sets for rtpengine

--- custom_scripts/rtpengine_inserts.sql
+++ custom_scripts/rtpengine_inserts.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO rtpengine (setid, url, weight, disabled) values(1, 'udp:172.25.0.30:2223', 0, 0);
+INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:172.25.0.30:2223', 2, 0, now());
 
-INSERT INTO rtpengine (setid, url, weight, disabled) values(2, 'udp:172.25.0.31:2223', 0, 0);
+INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:172.25.0.31:2223', 1, 0, now());
