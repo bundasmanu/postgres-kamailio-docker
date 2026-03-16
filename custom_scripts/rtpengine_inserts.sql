032a2a81dfb22827fc7bf2b206829b069f3d6f80
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2026-03-16 18:33:17 +0000

    feat: add new table ht_rtp_profiles

--- custom_scripts/rtpengine_inserts.sql
+++ custom_scripts/rtpengine_inserts.sql
@@ -2,3 +2,14 @@
 INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:172.25.0.30:2223', 2, 0, now());
 
 INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:172.25.0.31:2223', 1, 0, now());
+
+CREATE TABLE IF NOT EXISTS ht_rtp_profiles (
+    id SERIAL PRIMARY KEY NOT NULL,
+    key_name VARCHAR(64) DEFAULT '' NOT NULL,
+    key_type INTEGER DEFAULT 0 NOT NULL,
+    value_type INTEGER DEFAULT 0 NOT NULL,
+    key_value VARCHAR(256) DEFAULT '' NOT NULL,
+    expires INTEGER DEFAULT 0 NOT NULL
+);
+
+INSERT INTO ht_rtp_profiles (key_name, key_value) values('1', 'internal=public-internal;external=public-external');
