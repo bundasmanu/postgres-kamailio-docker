d13297c80dfecf5c91a86e5b06159b345064c7fc
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2026-05-07 19:45:25 +0100

    feat: update dispatcher entries

--- custom_scripts/dispatcher_inserts.sql
+++ custom_scripts/dispatcher_inserts.sql
@@ -1,8 +1,11 @@
 
-INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(1, 'sip:172.25.0.10:5060', 0, 0, 'type=external', 'Provider A'); -- for Asterisk use 172.25.0.20
+##INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(1, 'sip:172.25.0.10:5060', 0, 0, 'type=external', 'Provider A'); -- for Asterisk use 172.25.0.20
 
-INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(1, 'sip:172.25.0.11:5060', 0, 0, 'type=external', 'Provider B');
+##INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(1, 'sip:172.25.0.11:5060', 0, 0, 'type=external', 'Provider B');
 
-INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(2, 'sip:172.25.0.12:5060', 0, 0, 'type=internal', 'Internal A');
+##INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(2, 'sip:172.25.0.12:5060', 0, 0, 'type=internal', 'Internal A');
 
-INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(2, 'sip:172.25.0.13:5060', 0, 0, 'type=internal', 'Internal B');
+##INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(2, 'sip:172.25.0.13:5060', 0, 0, 'type=internal', 'Internal B');
+
+INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(1, 'sip:registrar-server.kamailio.svc.cluster.local:5060', 0, 0, 'sockname=lan-internal', 'Registrar Server Pool');
+INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(3, 'sip:webrtc-gateway.kamailio.svc.cluster.local:5060', 0, 0, 'sockname=lan-internal', 'WebRTC Gateway Pool');
