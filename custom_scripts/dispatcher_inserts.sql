22aa6747b5dfcf5a002b1e6a8addac5174f71222
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2026-05-18 16:14:10 +0100

    feat: add new dispatcher entries for core and b2bua

--- custom_scripts/dispatcher_inserts.sql
+++ custom_scripts/dispatcher_inserts.sql
@@ -8,12 +8,20 @@
 -- INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(2, 'sip:172.25.0.13:5060', 0, 0, 'type=internal', 'Internal B');
 
 INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(1, 'sip:registrar-server.kamailio.svc.cluster.local:5060', 0, 0, 'view=webrtc_gateway;sockname=lan-internal', 'Registrar Server Pool');
-INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(3, 'sip:webrtc-gateway.kamailio.svc.cluster.local:5060', 0, 0, 'view=registrar;sockname=lan-internal', 'WebRTC Gateway Pool');
+INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(2, 'sip:kamailio-core.kamailio.svc.cluster.local:5060', 0, 0, 'view=webrtc_gateway,b2bua;sockname=lan-internal', 'Kamailio Core Pool');
+INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(3, 'sip:webrtc-gateway.kamailio.svc.cluster.local:5060', 0, 0, 'view=registrar,core;sockname=lan-internal', 'WebRTC Gateway Pool');
+INSERT INTO dispatcher (setid, destination, flags, priority, attrs, description) values(4, 'sip:b2bua.b2bua.svc.cluster.local:5060', 0, 0, 'view=core;sockname=lan-internal', 'B2BUA Pool');
 
 CREATE OR REPLACE VIEW dispatcher_registrar AS
 SELECT *
 FROM dispatcher
 WHERE attrs ~ '(^|;)view=[^;]*\mregistrar\M';
+
+
+CREATE OR REPLACE  VIEW dispatcher_core AS
+SELECT *
+FROM dispatcher
+WHERE attrs ~ '(^|;)view=[^;]*\mcore\M';
 
 CREATE OR REPLACE  VIEW dispatcher_webrtc_gateway AS
 SELECT *
@@ -21,4 +29,5 @@ FROM dispatcher
 WHERE attrs ~ '(^|;)view=[^;]*\mwebrtc_gateway\M';
 
 INSERT INTO version (table_name, table_version) values ('dispatcher_registrar','4');
+INSERT INTO version (table_name, table_version) values ('dispatcher_core','4');
 INSERT INTO version (table_name, table_version) values ('dispatcher_webrtc_gateway','4');
