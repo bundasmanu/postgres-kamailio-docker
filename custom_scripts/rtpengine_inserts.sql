a7f5c20473cdb1b727548b17deba7f0144c16a0e
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2026-05-20 16:03:00 +0100

    feat: update rtpengine url and templates

--- custom_scripts/rtpengine_inserts.sql
+++ custom_scripts/rtpengine_inserts.sql
@@ -1,7 +1,5 @@
 
-INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:172.25.0.30:2223', 2, 0, now());
-
-INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:172.25.0.31:2223', 1, 0, now());
+INSERT INTO rtpengine (setid, url, weight, disabled, stamp) values(1, 'udp:rtpengine-0.rtpengine.rtpengine.svc.cluster.local:2223', 2, 0, now());
 
 CREATE TABLE IF NOT EXISTS ht_rtp_profiles (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -12,4 +10,4 @@ CREATE TABLE IF NOT EXISTS ht_rtp_profiles (
     expires INTEGER DEFAULT 0 NOT NULL
 );
 
-INSERT INTO ht_rtp_profiles (key_name, key_value) values('1', 'internal=public-internal;external=public-external');
+INSERT INTO ht_rtp_profiles (key_name, key_value) values('1', 'internal=public-internal;external=WebRTC');
